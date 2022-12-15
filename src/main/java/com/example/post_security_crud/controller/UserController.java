e5adea266d25d31a7530c0e8f4cc3c6622939fa6
hjun0917 <hjun0917@naver.com>
2022-12-15 21:36:21 +0900

    생성자 순서 수정

--- src/main/java/com/example/post_security_crud/controller/UserController.java
+++ src/main/java/com/example/post_security_crud/controller/UserController.java
@@ -6,14 +6,11 @@ import com.example.post_security_crud.dto.SignupRequestDto;
 import com.example.post_security_crud.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -27,7 +24,7 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseDto signup(@RequestBody SignupRequestDto signupRequestDto, HttpServletResponse response) {
         userService.signup(signupRequestDto);
-        return new ResponseDto("회원가입 성공", HttpStatus.OK.value());
+        return new ResponseDto( HttpStatus.OK.value(), "회원가입 성공");
     }
 
     //아래 @AuthenticationPrincipal <- 이거가 굉장히 많은 일을 해준다(코드를 많이 줄여줌)
